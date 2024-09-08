641446bb9041808c4e85d20a08d59f94b3b9c0ae
Emil Bertlin <emilbertlin0@gmail.com>
2024-09-08 20:33:25 +0200

    some basic refactoring

--- gitignore.cpp
+++ gitignore.cpp
@@ -24,30 +24,30 @@ bool isGitFolderInDir(const std::string &path) {
 }
 
 void writeToFile(const std::string &path, std::string &msg) {
-  if (std::filesystem::is_directory(msg) && msg.back() != '/') {
-    msg += "/"; // for correct formatting inside .gitignore
-  }
-  std::ifstream infile(path);
-  std::unordered_set<std::string> lines;
-  std::string line;
-  bool found = false;
-  while (std::getline(infile, line)) {
-    if (line == msg) {
-      found = true;
-      break;
+    // modify 'msg' directly if it's a directory
+    if (std::filesystem::is_directory(msg) && msg.back() != '/') {
+        msg += "/";
     }
-  }
-  if (found) {
-    std::cout << msg << " is already in " << path << std::endl;
-    return;
-  }
-  std::ofstream outfile(path, std::ios_base::app);
-  if (!outfile) {
-    printError("could not open or create " + path);
-    return;
-  }
-  outfile << msg << std::endl;
-  std::cout << "Added " << msg << " to " << path << std::endl;
+    std::fstream file(path, std::ios::in | std::ios::out | std::ios::app);
+    if (!file) {
+        printError("could not open " + path);
+        return;
+    }
+
+    // read file to an unordered_set for fast lookup
+    std::unordered_set<std::string> lines;
+    std::string line;
+    while (std::getline(file, line)) {
+        lines.insert(line);
+    }
+    if (lines.find(msg) != lines.end()) {
+        std::cout << msg << " is already in " << path << std::endl;
+        return;
+    }
+    file.clear(); // clear err flags
+    file.seekp(0, std::ios::end); // move the write pointer to the end
+    file << msg << std::endl;
+    std::cout << "Added " << msg << " to " << path << std::endl;
 }
 
 std::string getRelativePath(const std::string &from, const std::string &to) {
